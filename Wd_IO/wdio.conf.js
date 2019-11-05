93ae40c6ddd85ba12ae4804e311a2690206fae0d
sumanthvarma19 <56889707+sumanthvarma19@users.noreply.github.com>
2019-11-05 17:32:26 -0600

    Update wdio.conf.js

--- Wd_IO/wdio.conf.js
+++ Wd_IO/wdio.conf.js
@@ -95,7 +95,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://dlr-web-qa.dealerware.com/#/login',
+    baseUrl:'https://www.khanacademy.org/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 50000,
