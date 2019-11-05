7bc17fd4655bfe31537608ee9da1ad297a49af50
sumanthvarma19 <56889707+sumanthvarma19@users.noreply.github.com>
2019-11-05 17:30:03 -0600

    Update Home.page.js

--- Wd_IO/page/Home.page.js
+++ Wd_IO/page/Home.page.js
@@ -1,121 +1,230 @@
-class Home {
 
 
-    get navigationHeader() {
-        return $(".nav-header button");
-    }
+class SignUp{
 
-    get dashboardLink() {
-        return $("//a[text()='Dashboard']");
-    }
+  
+  get SignInbutton()
+  {
+    return $("//a[text()='Login']");
+  }
+  get SIgnupbutton(){
+    return $("//a[text()='Sign up']");
+  }
+  
+  get donateHeaderButton(){
+  
+    return $("(//a[text()='Donate'])[last()-1]");
+  }
+  
+  get lastDonateButton(){
+  
+    return $("(//a[text()='Donate'])[last()]");
+  }
+  
+  get learnerButton(){
 
-    get usersLink() {
-        return $("//a[text()='Users']");
-    }
+    return $("//button[text()='Learner']");
+  }
+ get teacherButton(){
+
+  return $("//button[text()='Teacher']");
+ }
+ get parentButton(){
+
+  return $("//button[text()='Parent']");
+ }
+
+ get monthPicker(){
+
+  return $("//select[@name='birth[month]']");
+ }
+ get dayPicker(){
+
+  return $("//select[@name='birth[day]']");
+ }
+
+get yearPicker(){
+
+  return $("//select[@name='birth[year]']");
+ }
+ get signUpbyusername(){
+
+  return $("//div[text()='Sign up by choosing a username']");
+ }
+ get signupEmail(){
+
+  return $("//input[@type='email']");
+ }
+
+ get signupuserName(){
+
+  return $("//input[@type='text']");
+ }
+
+ get createPassword(){
+   return $("//input[@type='password']");
+ }
+ get postsignup(){
+
+  return $("//div[text()='Sign up']");
+ }
+
+get UserName(){
+
+  return $("//span[@data-test-id='header-profile-button']/span");
+}
+
+get logoutButton(){
+
+  return $("//a[text()='Log out']");
+}
+
+get classname(){
+
+  return $("label[data-test-id='grade-picker-item-0-11']>div");
+}
+
+get continuebutton(){
+
+  return $("(//button//div[text()='Continue'])[1]");
+}
+get coursename(){
+  return $("//label[contains(.,'Grammar')]//input");
+}
+
+get clickContinuecoursebutton(){
+
+  return $("//button[contains(.,'Continue with')]//div");
+}
+////////////////////////////////////////////
+
+ clickSignupButton ()
+ {
+     browser.waitUntil(() => this.SIgnupbutton.isDisplayed());
+     this.SIgnupbutton.click();
+ }
+
+ clickparentButton(){
+  browser.waitUntil(() => this.parentButton.isDisplayed());
+  this.parentButton.click();
+}
+clickTeacherButton(){
+  browser.waitUntil(() => this.teacherButton.isDisplayed());
+  this.teacherButton.click();
+
+}
+clickLearnerButton(){
+  browser.waitUntil(() => this.learnerButton.isDisplayed());
+  this.learnerButton.click();
+}
+clickMonthpicker(){
+  browser.waitUntil(() => this.monthPicker.isDisplayed());
+  this.monthPicker.click();
+  this.monthPicker.selectByIndex(8);
+}
+clickdaypicker(){
+  browser.waitUntil(() => this.dayPicker.isDisplayed());
+  this.dayPicker.click();
+  this.dayPicker.selectByIndex(8);
+}
+
+clickyearpicker(){
+  browser.waitUntil(() => this.yearPicker.isDisplayed());
+  this.yearPicker.click();
+  this.yearPicker.selectByIndex(8);
+}
+
+clicksignupbyuserName(){
+  browser.waitUntil(() => this.signUpbyusername.isDisplayed());
+  this.signUpbyusername.click();
+}
+entersignupEmail(email){
+  browser.waitUntil(() => this.signupEmail.isDisplayed());
+  this.signupEmail.click();
+  this.signupEmail.clearValue();
+  this.signupEmail.setValue(email);
+
+}
+enterSignupUserName(name){
+  browser.waitUntil(() => this.signupuserName.isDisplayed());
+  this.signupuserName.click();
+  this.signupuserName.clearValue();
+  this.signupuserName.setValue(name);
+
+}
+enterSignPassword(password){
+  browser.waitUntil(() => this.createPassword.isDisplayed());
+  this.createPassword.click();
+  this.createPassword.clearValue();
+  this.createPassword.setValue(password);
+
+}
+clickPostSignUp(){
+  this.postsignup.click();
+}
 
 
-    get reservationsLink() {
-        return $("//a[text()='Reservations']");
-    }
 
-    get carsLink() {
-        return $("//a[text()='Cars']");
-    }
+clickClassName(){
+  browser.execute(function () {
+    document.querySelector('label[data-test-id="grade-picker-item-0-11"]').scrollIntoView();
+  });
+  this.classname.click();
+}
 
-    get locationDetailsLink() {
-        return $("//a[text()='Location Details']");
-    }
+clickContinueButton(){
+  browser.waitUntil(() => this.continuebutton.isDisplayed());
+  this.continuebutton.click();
+}
+clickCourseName(){
+  browser.execute(function () {
+    document.querySelector('div[class="_wgmchy"]:nth-child(4)').scrollIntoView();
+  });
+  browser.pause(2000);
+  this.coursename.click();
+}
 
-    get reportsLink() {
-        return $("//a[text()='Reports']");
-    }
+clickCourseContinueButton(){
+  browser.waitUntil(() => this.clickContinuecoursebutton.isDisplayed());
+  this.clickContinuecoursebutton.click();
+}
 
-    get promotionsLink() {
-        return $("//a[text()='Promotions']");
-    }
+clickUserNameText(username){
+  browser.waitUntil(() => this.UserName.isDisplayed());
+  expect(this.UserName.getText()).toBe(username, "user name is not getting displayed");
+  this.UserName.click();
+}
+clickLogoutButton(){
+  browser.waitUntil(() => this.logoutButton.isDisplayed());
+  this.logoutButton.click();
+}
+/////////////////////////////////////
+ validateSignup(email, name, password, username){
 
-    get logOutLink() {
-        return $("//a[text()='Log Out']");
-    }
+  this.clickSignupButton();
+  this.clickparentButton();
+  this.clickTeacherButton();
+  this.clickLearnerButton();
+ this.clickMonthpicker();
+ this.clickdaypicker();
+ this.clickyearpicker();
+ this.clicksignupbyuserName();
+ this.entersignupEmail(email);
+ this.enterSignupUserName(name);
+ this.enterSignPassword(password);
+this.clickPostSignUp();
+browser.pause(10000);
+
+this.clickClassName();
+this.clickContinueButton();
+this.clickCourseName();
+this.clickCourseContinueButton();
+browser.pause(3000);
+this.clickUserNameText(username);
+this.clickLogoutButton();
+ }
 
 
 
+}module.exports = SignUp;
 
-    clickNavHeader() {
-        this.navigationHeader.isDisplayed();
-        this.navigationHeader.click();
-    }
-
-
-    waitForPageTOLoad() {
-        for (let i = 0; i < 5; i++) {
-            var oldSource = browser.getPageSource();
-            browser.pause(2000);
-            if (oldSource === browser.getPageSource()) {
-                break;
-            }
-        }
-    }
-
-/* 
-    method Name : navigateToSideLinks
-    return type : void 
-    Parameters : method will accept one parameter i.e linkname like "dashboard","cars"...etc
-    Purpose : purpose of this method is used to naviage left side navigation links in application .
-*/
-
-    navigateToSideLinks(linkName) {
-        this.navigationHeader.isDisplayed();
-        this.navigationHeader.click();
-        this.waitForPageTOLoad();
-        switch (linkName) {
-            case "dashboard":
-                browser.waitUntil(() => this.dashboardLink.isDisplayed());
-                this.dashboardLink.click();
-                this.waitForPageTOLoad();
-                break;
-            case "users":
-                browser.waitUntil(() => this.usersLink.isDisplayed());
-                this.usersLink.click();
-                this.waitForPageTOLoad();
-                break;
-            case "reservations":
-                browser.waitUntil(() => this.reservationsLink.isDisplayed());
-                this.reservationsLink.click();
-                this.waitForPageTOLoad();
-                break;
-            case "cars":
-                browser.waitUntil(() => this.carsLink.isDisplayed());
-                this.carsLink.click();
-                this.waitForPageTOLoad();
-                break;
-
-            case "locationDetails":
-                browser.waitUntil(() => this.locationDetailsLink.isDisplayed());
-                this.locationDetailsLink.click();
-                this.waitForPageTOLoad();
-                break;
-
-            case "reports":
-                browser.waitUntil(() => this.reportsLink.isDisplayed());
-                this.reportsLink.click();
-                this.waitForPageTOLoad();
-                break;
-
-            case "promotions":
-                browser.waitUntil(() => this.promotionsLink.isDisplayed());
-                this.promotionsLink.click();
-                this.waitForPageTOLoad();
-                break;
-
-            case "logout":
-                browser.waitUntil(() => this.logOutLink.isDisplayed());
-                this.logOutLink.click();
-                this.waitForPageTOLoad();
-                break;
-
-        }
-
-    }
-
-} module.exports = Home;
