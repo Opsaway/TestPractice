8be68534e05065eabd80caff9ab66f6577d34478
sumanthvarma19 <56889707+sumanthvarma19@users.noreply.github.com>
2019-11-05 17:30:55 -0600

    Update login.spec.js

--- Wd_IO/spec/login.spec.js
+++ Wd_IO/spec/login.spec.js
@@ -1,34 +1,23 @@
 
 require("dotenv").config();
 
-const Login = require("../page/login.page.js");
-const Home= require("../page/Home.page.js");
-const LocationDetails= require("../page/locationDetails.page.js");
 
-const login = new Login();
-const home= new Home();
-const locDeatils= new LocationDetails();
-let envUrl;
-if(process.env.ENVIROMENT == "QA"){
-  envUrl=process.env.QA_URL;
-}else{
-  envUrl=process.env.STG_URL;
-}
+const SignUp = require("../page/Home.Page.js");
+
+const signup = new SignUp();
 
 
+const envUrl = process.env.WebUrl;
+const email = "sumanthvarma"+ Math.floor(Math.random() * 999999)+"@gmail.com";
+const password = process.env.PassWord;
+const name = "sumanth"+ Math.floor(Math.random() * 999999)
 
-describe("Validating login functionality ", () => {
-
-  it("test_login With valid details", () => {
-    browser.url(envUrl);
-    login.login(process.env.adminUser,process.env.adminPassword,true);
-    expect(true).toBe(login.pageTitle.getText().includes('Hello,'), "user not logged in to application");
-    home.navigateToSideLinks("locationDetails");
-    locDeatils.selectLocation("Austin");
-    locDeatils.clickFleetMgmtTab();
-  });
-
-
-})
+describe("Validating Signup functionality", () => {
+  it("validating getstarted click", () => {
+    browser.url(envUrl);                                                                    
+    signup.validateSignup(email, name, password, name);
+      });
 
 
+
+  })
